a130560b6625df17096a9c42aee8e2c34a8b8d2f
David-Isaac-A-M <153351913+David-Isaac-A-M@users.noreply.github.com>
2025-04-20 16:40:15 -0700

    Respaldo de seguridad enemigos
    Hay que hacer las animaciones de los enemigos en un controllador diferente para añadir eventos, asi que en caso de que este proceso rompa lo ya hecho en el enemigo hago un commit de respaldo.

---
+++ Assets/Scripts/Enemigos/Enemigo_CQC.cs
@@ -0,0 +1,72 @@
+//using Unity.Mathematics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo_CQC : MonoBehaviour
+{
+    public int rutina;
+    public float cronometro;
+    public Animator animator;
+    public Quaternion angulo;
+    public float grado;
+
+    
+    public GameObject jugador;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        jugador = GameObject.FindWithTag("jugador");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Comportamiento_Enemigo();
+    }
+
+    public void Comportamiento_Enemigo()
+    {
+        if(Vector3.Distance(transform.position, jugador.transform.position)>5)
+        {
+            Debug.Log("Estoy en el if");
+            animator.SetBool("rage", false);
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro > 4)
+            {
+                rutina = Random.Range(0, 2);
+                cronometro = 0;
+            }
+
+            switch (rutina)
+            {
+                case 0:
+                    animator.SetBool("walk", false);
+                    break;
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    animator.SetBool("walk", true);
+                    break;
+            }
+        }
+        else
+        {
+            Debug.Log("Estoy en el else");
+            var lookpos = jugador.transform.position - transform.position;
+            lookpos.y = 0;
+            var rotation = Quaternion.LookRotation(lookpos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
+            animator.SetBool("walk", true);
+            animator.SetBool("rage", true);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        }
+    }
+}
